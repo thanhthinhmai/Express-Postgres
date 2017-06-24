106334e5067c845df15aa8ea506557312f59797c
thanhthinhmai <thanhthinhmai@gmail.com>
2017-06-24 23:35:04 +0700

    ToDo Cart

--- app.js
+++ app.js
@@ -31,3 +31,5 @@ app.get('/', routes.home);
 
 app.get('/star_wars_episode/:episode_number', routes.movie_single);
 
+app.get('/*', routes.notFound)
+
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const { db, } = require('../pgp');
 exports.home = (req,res) =>{
     db.any('SELECT * FROM movies')
     .then( data =>{
-        console.log(data);
+        // console.log(data);
 
         res.render('home',{
             title : 'Star Wars Movies App',
@@ -17,10 +17,20 @@ exports.movie_single = (req, res) =>{
     let episode_number = req.params.episode_number;
     db.any('SELECT * FROM movies WHERE id = ' + episode_number)
     .then( data =>{
+        if(episode_number >= 1 && episode_number < 7){
         res.render('movie_single',{
             movies:data
         })
-    
+        }else{
+            res.render('notFound',{
+                title: 'Not page'
+            })
+        }
     })
 }
 
+exports.notFound = (req, res) =>{
+    res.render('notFound',{
+        title : 'Not page '
+    })
+}
